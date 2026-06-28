b03fa665b1497a9d6ac707adb2c85712ee4d0b1c
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-28 03:30:10 -0400

    refactor: add core simulation kit entrypoint

---
+++ src/core-kits/core-simulation-kit/index.js
@@ -0,0 +1,4 @@
+export { TimingWindowState, TimingWindowAction, TimingWindowResolved, createTimingWindowKit, gradeTimingWindow } from "../../timing-window-kit.js";
+export { ResourcePressureState, ResourcePressureAdjust, ResourcePressureChanged, ResourcePressureDepleted, createResourcePressureKit, pressureValue } from "../../resource-pressure-kit.js";
+export { ObjectiveFlowState, ObjectiveFlowAction, ObjectiveFlowReset, ObjectiveFlowCompleted, ObjectiveStepCompleted, createObjectiveFlowKit } from "../../objective-flow-kit.js";
+export { createCoreSimulationKit } from "../core-domains.js";

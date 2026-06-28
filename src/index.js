58a4e116cfd22098be7386d9fec4e00a3e0c55a4
thecrimsondeveloper <110572544+thecrimsondeveloper@users.noreply.github.com>
2026-06-28 03:32:57 -0400

    feat: expose foundation and core capability kits

--- src/index.js
+++ src/index.js
@@ -1,548 +1,83 @@
-export {
-  DEFAULT_PHASES,
-  createCollisionSystem,
-  createDamageSystem,
-  createDeathSystem,
-  createDespawnSystem,
-  createInputSystem,
-  createMovementSystem,
-  createScheduler,
-  createWorld,
-  defineComponent,
-  defineEvent,
-  defineResource
-} from "./ecs.js";
-
-export {
-  createEventSurface,
-  createLifecycleSurface,
-  createQuerySurface,
-  createResourceSurface
-} from "./surfaces.js";
-
-export {
-  createEngine,
-  createRealtimeEngine
-} from "./engine.js";
-
-export {
-  defineRuntimeKit,
-  installRuntimeKit,
-  validateRuntimeKit
-} from "./runtime-kit.js";
-
-export {
-  DOMAIN_SERVICE_NAMESPACE,
-  createDomainServiceToken,
-  defineDomainServiceKit,
-  extendDomainServiceKit,
-  isDomainServiceKit,
-  validateDomainServiceKit
-} from "./domain-service-kit.js";
-
-export {
-  CommonGameComponents,
-  CommonGameEvents,
-  CommonGameResources,
-  commonGameDefinitions,
-  getCommonGameDefinitions
-} from "./common-game-definitions.js";
-
-export {
-  createGameKitComposer,
-  createRealtimeGame
-} from "./game-kit-composer.js";
-
-export {
-  createHostGraphSnapshot,
-  createNexusHost,
-  defineHostAdapter
-} from "./host.js";
-
-export {
-  createCanvas2DRenderer,
-  createCustomWebGLRenderer,
-  createHeadlessRenderer,
-  createRenderer,
-  createThreeRenderer
-} from "./renderers.js";
-
-export {
-  createMaterialRegistry,
-  createShaderRegistry,
-  fishingShaders
-} from "./shaders.js";
-
-export {
-  BaseSequence,
-  CompletionMode,
-  DefaultSequenceRegistry,
-  EntityControllerSequence,
-  GroupSequence,
-  SequenceLifecycle,
-  UIControllerSequence,
-  WaitForEventSequence,
-  WaitForSequence,
-  createSequenceRuntime
-} from "./sequences.js";
-
-export {
-  SequenceNodeCompletionMode,
-  SequenceNodeDriver,
-  SequenceNodeEventType,
-  SequenceNodeState,
-  applySequenceWrites,
-  createSequenceNodeEvent,
-  createSequenceNodeEventBus,
-  createSequenceNodePlan,
-  createSequenceNodeRuntime,
-  evaluateSequenceCondition,
-  flattenSequenceNode,
-  normalizeSequenceNode,
-  readSequencePath,
-  validateSequenceNode,
-  writeSequencePath
-} from "./sequence-node.js";
-
-export {
-  BuiltInSequenceNodeTypes,
-  createDefaultSequenceNodeLibrary,
-  createSequenceNodeLibrary,
-  defineSequenceNodeType
-} from "./sequence-node-library.js";
-
-export {
-  createSequenceNodeKit,
-  deploySequenceNode,
-  installSequenceNodeKits
-} from "./sequence-node-kit.js";
-
-export {
-  createFishingKit
-} from "./fishing-kit.js";
-
-export {
-  createReefRescueKit
-} from "./reef-rescue-kit.js";
-
-export {
-  createRealismKit,
-  realismPresets,
-  qualityProfiles
-} from "./realism-kit.js";
-
-export {
-  createTerrainKit,
-  createTerrainQuery,
-  terrainLayers
-} from "./terrain-kit.js";
-
-export {
-  createProceduralKit,
-  createProceduralQuery,
-  createProceduralSnapshot,
-  proceduralAlgorithms
-} from "./procedural-kit.js";
-
-export {
-  create3DNavigationGraph,
-  createNavMeshFromWalkability,
-  createNavMeshKit
-} from "./navmesh-kit.js";
-
-export {
-  createAStarPathfinder,
-  createNavigationQuery,
-  createPathfindingKit,
-  navigationAdapters,
-  navigationHeuristics
-} from "./pathfinding-kit.js";
-
-export {
-  createPhysicsKit,
-  createWorldPhysicsKit
-} from "./world-physics-kit.js";
-
-export {
-  ARAnchor,
-  ARAnchorPlaced,
-  ARExperienceCompleted,
-  ARInputState,
-  ARInteractionTarget,
-  ARObjectTapped,
-  ARPlane,
-  ARPlaneDetected,
-  ARPlacedObject,
-  ARPlacementState,
-  ARReticle,
-  ARSessionFailed,
-  ARSessionStarted,
-  ARSessionState,
-  ARStepCompleted,
-  ARStepState,
-  ARSupportState,
-  arComponents,
-  arEvents,
-  arResources,
-  createARKit
-} from "./ar-kit.js";
-
-export {
-  createHitTestSource,
-  detectARSupport,
-  endARSession,
-  requestARSession,
-  resolveHitTestPose
-} from "./ar-session.js";
-
-export {
-  createARRenderer
-} from "./ar-renderer.js";
-
-export {
-  ARExperienceAction,
-  ARExperienceReset,
-  ARExperienceState,
-  createARExperienceKit
-} from "./ar-experience-kit.js";
-
-export {
-  chooseARMode,
-  classifyARDevice
-} from "./ar-device.js";
-
-export {
-  createARLaunchRuntime
-} from "./ar-launcher.js";
-
-export {
-  createActionMovementKit,
-  createLocomotionKit
-} from "./action-movement-kit.js";
-
-export {
-  createCharacterMovementKit
-} from "./action-movement-kit.js";
-
-export {
-  createCharacterInteractionKit,
-  createInteractionKit
-} from "./interaction-kit.js";
-
-export {
-  createLightCombatKit
-} from "./light-combat-kit.js";
-
-export {
-  createCompanionCommandKit
-} from "./companion-command-kit.js";
-
-export {
-  createCameraKit,
-  createCharacterCameraKit
-} from "./character-camera-kit.js";
-
-export {
-  createCameraOcclusionKit
-} from "./camera-occlusion-kit.js";
-
-export {
-  createCharacterRagdollKit
-} from "./character-ragdoll-kit.js";
-
-export {
-  createForestPlacementKit
-} from "./forest-placement-kit.js";
-
-export {
-  createShrinePuzzleKit
-} from "./shrine-puzzle-kit.js";
-
-export {
-  createCorruptionWorldKit
-} from "./corruption-world-kit.js";
-
-export {
-  createTreeRunnerKit
-} from "./tree-runner-kit.js";
-
-export {
-  ObjectiveFlowAction,
-  ObjectiveFlowCompleted,
-  ObjectiveFlowReset,
-  ObjectiveFlowState,
-  ObjectiveStepCompleted,
-  createObjectiveFlowKit
-} from "./objective-flow-kit.js";
-
-export {
-  SpatialRoomState,
-  createSpatialRoomKit,
-  normalizeBuildingDataset
-} from "./spatial-room-kit.js";
-
-export {
-  createGreyboxBuilding,
-  createGreyboxBuildingKit
-} from "./greybox-building-kit.js";
-
-export {
-  SurfacePlacementConfig,
-  createSurfacePlacementKit
-} from "./surface-placement-kit.js";
-
-export {
-  InteractionTargetCompleted,
-  InteractionTargetInput,
-  InteractionTargetState,
-  createInteractionTargetKit
-} from "./interaction-target-kit.js";
-
-export {
-  CollectibleClaimed,
-  CollectibleState,
-  createCollectibleKit
-} from "./collectible-kit.js";
-
-export {
-  SymbolAlignmentState,
-  createSymbolAlignmentKit
-} from "./symbol-alignment-kit.js";
-
-export {
-  SortingState,
-  createSortingKit
-} from "./sorting-kit.js";
-
-export {
-  MicroPlatformerState,
-  createMicroPlatformerKit
-} from "./micro-platformer-kit.js";
-
-export {
-  RevealLightState,
-  createRevealLightKit
-} from "./reveal-light-kit.js";
-
-export {
-  MovingTargetState,
-  createMovingTargetKit
-} from "./moving-target-kit.js";
-
-export {
-  LockAndSocketState,
-  createLockAndSocketKit
-} from "./lock-and-socket-kit.js";
-
-export {
-  RenderDescriptorState,
-  createRenderDescriptorKit,
-  createRenderDescriptorSnapshot
-} from "./render-descriptor-kit.js";
-
-export {
-  ScheduleCycle,
-  ScheduleState,
-  createScheduleKit
-} from "./schedule-kit.js";
-
-export {
-  EconomyState,
-  EconomyTransactionCompleted,
-  EconomyTransactionRejected,
-  EconomyTransactionRequest,
-  createEconomyKit
-} from "./economy-kit.js";
-
-export {
-  LifecycleProgressionCompleted,
-  LifecycleProgressionStart,
-  LifecycleProgressionState,
-  createLifecycleProgressionKit
-} from "./lifecycle-progression-kit.js";
-
-export {
-  FacilityConditionChanged,
-  FacilityOperationsState,
-  FacilityOutputProduced,
-  createFacilityOperationsKit
-} from "./facility-operations-kit.js";
-
-export {
-  OccupantAbandoned,
-  OccupantFlowState,
-  OccupantNeedCreated,
-  OccupantServed,
-  OccupantSpawn,
-  createOccupantFlowKit
-} from "./occupant-flow-kit.js";
-
-export {
-  TransportRouteArrived,
-  TransportRouteCall,
-  TransportRouteState,
-  createTransportRouteKit
-} from "./transport-route-kit.js";
-
-export {
-  RequestQueueAdd,
-  RequestQueueExpired,
-  RequestQueueFulfill,
-  RequestQueueFulfilled,
-  RequestQueueState,
-  createRequestQueueKit
-} from "./request-queue-kit.js";
-
-export {
-  TelemetryState,
-  createTelemetryKit
-} from "./telemetry-kit.js";
-
-export {
-  TimingWindowAction,
-  TimingWindowResolved,
-  TimingWindowState,
-  createTimingWindowKit,
-  gradeTimingWindow
-} from "./timing-window-kit.js";
-
-export {
-  ResourcePressureAdjust,
-  ResourcePressureChanged,
-  ResourcePressureDepleted,
-  ResourcePressureState,
-  createResourcePressureKit,
-  pressureValue
-} from "./resource-pressure-kit.js";
-
-export {
-  HazardFieldCollision,
-  HazardFieldSpawned,
-  HazardFieldState,
-  createHazardFieldKit,
-  queryHazardCircle
-} from "./hazard-field-kit.js";
-
-export {
-  CargoConditionChanged,
-  CargoConditionDepleted,
-  CargoDeposited,
-  CargoManifestState,
-  CargoPickedUp,
-  CargoQuotaCompleted,
-  createCargoManifestKit,
-  queryNearestCargo
-} from "./cargo-manifest-kit.js";
-
-export {
-  RequestFulfillmentCompleted,
-  RequestFulfillmentCreated,
-  RequestFulfillmentExpired,
-  RequestFulfillmentState,
-  createRequestFulfillmentKit,
-  queryNearestOpenRequest
-} from "./request-fulfillment-kit.js";
-
-export {
-  PursuitPressureCaught,
-  PursuitPressureChanged,
-  PursuitPressureRecovered,
-  PursuitPressureState,
-  createPursuitPressureKit,
-  pursuitBand
-} from "./pursuit-pressure-kit.js";
-
-export {
-  InputActionPressed,
-  InputIntentChanged,
-  InputIntentState,
-  createInputIntentKit
-} from "./input-intent-kit.js";
-
-export {
-  ScenarioCheckpointReached,
-  ScenarioDurationCompleted,
-  ScenarioDurationState,
-  createScenarioDurationKit
-} from "./scenario-duration-kit.js";
-
-export {
-  WaterSurfaceState,
-  createWaterSurfaceKit,
-  queryWaterSurface
-} from "./water-surface-kit.js";
-
-export {
-  VehicleDynamicsInput,
-  VehicleDynamicsState,
-  VehicleImpact,
-  createVehicleDynamicsKit
-} from "./vehicle-dynamics-kit.js";
-
-export {
-  AssistanceTargetAttached,
-  AssistanceTargetCompleted,
-  AssistanceTargetLost,
-  AssistanceTargetStabilized,
-  AssistanceTargetState,
-  createAssistanceTargetKit
-} from "./assistance-target-kit.js";
-
-export {
-  TransferZoneCompleted,
-  TransferZoneState,
-  createTransferZoneKit
-} from "./transfer-zone-kit.js";
-
-export {
-  RouteFieldState,
-  createRouteFieldKit,
-  queryNearestRouteMarker
-} from "./route-field-kit.js";
-
-export {
-  ScenarioDriverState,
-  createScenarioDriverKit
-} from "./scenario-driver-kit.js";
-
-export {
-  SpatialScaleAnchorEntered,
-  SpatialScaleBandChanged,
-  SpatialScaleState,
-  createSpatialScaleKit,
-  queryNearestScaleAnchor
-} from "./spatial-scale-kit.js";
-
-export {
-  LandmarkActivated,
-  LandmarkDiscovered,
-  LandmarkGuidanceState,
-  LandmarkReached,
-  createLandmarkGuidanceKit,
-  queryNearestLandmark
-} from "./landmark-guidance-kit.js";
-
-export {
-  AffordanceActivated,
-  AffordanceCompleted,
-  AffordanceEntered,
-  EnvironmentalAffordanceState,
-  createEnvironmentalAffordanceKit,
-  queryNearbyAffordances
-} from "./environmental-affordance-kit.js";
-
-export {
-  createWebXRPlaneMode
-} from "./ar-modes/webxr-plane.js";
-
-export {
-  createCameraOverlayMode
-} from "./ar-modes/camera-overlay.js";
-
-export {
-  createPageMarkerMode
-} from "./ar-modes/page-marker.js";
-
-export {
-  createFallbackPreviewMode
-} from "./ar-modes/fallback-preview.js";
+export * from "./ecs.js";
+export * from "./surfaces.js";
+export * from "./engine.js";
+export * from "./runtime-kit.js";
+export * from "./domain-service-kit.js";
+export * from "./common-game-definitions.js";
+export * from "./game-kit-composer.js";
+export * from "./host.js";
+export * from "./renderers.js";
+export * from "./shaders.js";
+export * from "./sequences.js";
+export * from "./sequence-node.js";
+export * from "./sequence-node-library.js";
+export * from "./sequence-node-kit.js";
+export * from "./foundation/index.js";
+export * from "./core-kits/index.js";
+export * from "./fishing-kit.js";
+export * from "./reef-rescue-kit.js";
+export * from "./realism-kit.js";
+export * from "./terrain-kit.js";
+export * from "./procedural-kit.js";
+export * from "./navmesh-kit.js";
+export * from "./pathfinding-kit.js";
+export * from "./world-physics-kit.js";
+export * from "./ar-kit.js";
+export * from "./ar-session.js";
+export * from "./ar-renderer.js";
+export * from "./ar-experience-kit.js";
+export * from "./ar-device.js";
+export * from "./ar-launcher.js";
+export * from "./action-movement-kit.js";
+export * from "./interaction-kit.js";
+export * from "./light-combat-kit.js";
+export * from "./companion-command-kit.js";
+export * from "./character-camera-kit.js";
+export * from "./camera-occlusion-kit.js";
+export * from "./character-ragdoll-kit.js";
+export * from "./forest-placement-kit.js";
+export * from "./shrine-puzzle-kit.js";
+export * from "./corruption-world-kit.js";
+export * from "./tree-runner-kit.js";
+export * from "./objective-flow-kit.js";
+export * from "./spatial-room-kit.js";
+export * from "./greybox-building-kit.js";
+export * from "./surface-placement-kit.js";
+export * from "./interaction-target-kit.js";
+export * from "./collectible-kit.js";
+export * from "./symbol-alignment-kit.js";
+export * from "./sorting-kit.js";
+export * from "./micro-platformer-kit.js";
+export * from "./reveal-light-kit.js";
+export * from "./moving-target-kit.js";
+export * from "./lock-and-socket-kit.js";
+export * from "./render-descriptor-kit.js";
+export * from "./schedule-kit.js";
+export * from "./economy-kit.js";
+export * from "./lifecycle-progression-kit.js";
+export * from "./facility-operations-kit.js";
+export * from "./occupant-flow-kit.js";
+export * from "./transport-route-kit.js";
+export * from "./request-queue-kit.js";
+export * from "./telemetry-kit.js";
+export * from "./timing-window-kit.js";
+export * from "./resource-pressure-kit.js";
+export * from "./hazard-field-kit.js";
+export * from "./cargo-manifest-kit.js";
+export * from "./request-fulfillment-kit.js";
+export * from "./pursuit-pressure-kit.js";
+export * from "./input-intent-kit.js";
+export * from "./scenario-duration-kit.js";
+export * from "./water-surface-kit.js";
+export * from "./vehicle-dynamics-kit.js";
+export * from "./assistance-target-kit.js";
+export * from "./transfer-zone-kit.js";
+export * from "./route-field-kit.js";
+export * from "./scenario-driver-kit.js";
+export * from "./spatial-scale-kit.js";
+export * from "./landmark-guidance-kit.js";
+export * from "./environmental-affordance-kit.js";
+export * from "./ar-modes/webxr-plane.js";
+export * from "./ar-modes/camera-overlay.js";
+export * from "./ar-modes/page-marker.js";
+export * from "./ar-modes/fallback-preview.js";
